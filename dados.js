e3cee699f067f8d9a6081f2355a023eed4b8ea4f
Kriska2024 <164671416+Kriska2024@users.noreply.github.com>
2024-09-04 21:38:15 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,25 @@
+
+
+let goblin = {
+    titulo : "Goblin",
+    descricao : "Goblin é uma emocionante série de fantasia e romance que conta a história de Kim Shin, um goblin imortal e protetor de almas humanas. Condenado a viver para sempre, ele busca uma noiva humana para quebrar sua maldição e encontrar a paz.",
+    link: "https://pt.wikipedia.org/wiki/Guardian:_The_Lonely_and_Great_God"
+}
+
+let UmaAdvogadaExtraordinaria = {
+    titulo : "Uma Advogada Extraordinária",
+    descricao : "Uma história inspiradora sobre uma advogada com Síndrome de Savant, que utiliza suas habilidades excepcionais para defender os mais vulneráveis.",
+    link: "https://www.exemplo.com/uma-advogada-extraordinaria"
+}
+
+let eTudoMeu = {
+    titulo : "É Tudo Meu",
+    descricao : "Uma trama envolvente sobre uma família rica e poderosa que luta pelo controle de um império empresarial, repleta de intrigas e reviravoltas.",
+    link: "https://www.exemplo.com/e-tudo-meu"
+}
+
+let dados = [
+    goblin,
+    UmaAdvogadaExtraordinaria,
+    eTudoMeu
+];
